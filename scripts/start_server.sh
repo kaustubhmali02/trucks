a19a407771f56417c05400dbe9b58d486f6ff025
Kaustubh Mali <kaustubhmali02@gmail.com>
2022-06-09 18:15:23 +0530

    changes in the start script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Checking if there are any running containers with same name
+
+if [ "$(docker ps -aq -f name=hello-world-deploy)" ];then
+    echo "Running Countainers. Removing them"
+    docker rm -f $(docker ps -aq -f name=hello-world-deploy)
+    if [ "$(docker ps -aq -f status=exited -f name=hello-world-deploy)" ]; then
+        # cleanup
+        docker rm -f $(docker ps -aq -f status=exited -f name=hello-world-deploy)
+    fi
+else
+    echo "Din't find any Running Containers"
+fi
+
 # Pulling latest image from docker hub
 docker pull kaustubhmali/hello-world-build:latest
 
