bf8e1524daf4fe811e411ad520142903cac50ffd
Kaustubh Mali <kaustubhmali02@gmail.com>
2022-06-09 16:41:08 +0530

    changes

--- ec2-instance-startup-script.sh
+++ ec2-instance-startup-script.sh
@@ -12,11 +12,11 @@ apt install -y ruby wget unzip ca-certificates curl gnupg lsb-release
 
 cd /home/ubuntu
 
-wget https://dlcdn.apache.org/maven/maven-3/3.8.5/binaries/apache-maven-3.8.5-bin.zip
-unzip apache-maven-3.8.5-bin.zip
-mv apache-maven-3.8.5 /opt
+# wget https://dlcdn.apache.org/maven/maven-3/3.8.5/binaries/apache-maven-3.8.5-bin.zip
+# unzip apache-maven-3.8.5-bin.zip
+# mv apache-maven-3.8.5 /opt
 
-echo export PATH=/opt/apache-maven-3.8.5/bin:$PATH | tee -a /etc/profile
+# echo export PATH=/opt/apache-maven-3.8.5/bin:$PATH | tee -a /etc/profile
 
 wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 
